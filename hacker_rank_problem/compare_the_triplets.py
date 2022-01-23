7cb899c5419925d81deaaf42b750db7741ef369d
shruti-bt <shruti.borkute312@gmail.com>
2022-01-23 12:54:19 +0530

    added compare the triplet file.

---
+++ hacker_rank_problem/compare_the_triplets.py
@@ -0,0 +1,25 @@
+import math
+import os
+import random
+import re
+import sys
+
+def compareTriplets(a, b):
+    count_a = 0
+    count_b = 0
+    for i in range(len(a)):
+        if a[i] > b[i]:
+            count_a += 1
+        elif a[i] == b[i]:
+            pass
+        elif a[i] < b[i]:
+            count_b += 1
+    return count_a, count_b
+    
+if __name__ == '__main__':
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
